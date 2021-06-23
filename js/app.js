937a0dbc9a2f61487be33125156e7b0e6235d272
Kai Conkling <kaiconkling@gmail.com>
2021-06-22 22:57:33 -0700

    Adding nav animation in js code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 // anonymous function
 const navSlide = () => {
 
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () =>  {
+        nav.classList.toggle('nav-active');
+    });
+
+    // Animate nav links
+    navLinks.forEach((link, index) => {
+        // remember use forward ticks
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;       
+    });
+
 }
+
+navSlide();

78fbdcb35090173189ae86f2a2cea77b99b5e1d9
Kai Conkling <kaiconkling@gmail.com>
2021-06-22 23:38:27 -0700

    Final code for responsive nav

--- js/app.js
+++ js/app.js
@@ -5,16 +5,29 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li')
 
+    // when user clicks on burger 
     burger.addEventListener('click', () =>  {
+
+        // Toggle navigation
         nav.classList.toggle('nav-active');
-    });
 
-    // Animate nav links
-    navLinks.forEach((link, index) => {
-        // remember use forward ticks
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;       
-    });
+        
+    
+        // Animate nav links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                // remember use forward ticks
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`; 
+            }
+                  
+        });
 
+        // Toggle burger
+        nav.classList.toggle('burger-active');
+    });
 }
 
 navSlide();
